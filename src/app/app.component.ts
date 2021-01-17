47912cee266456bd47ea1fd00ec061524b80ce7e
azizkale <azizkale@hotmail.com>
2021-01-17 22:14:58 +0100

    getUsesAdded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MyserviceService } from './myservice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'apollo-angular-in-angular';
+export class AppComponent implements OnInit {
+  users = [];
+
+  constructor(private myservice: MyserviceService) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.myservice.getUsers().subscribe((data) => {
+      this.users = data.data.users;
+      console.log(this.users);
+    });
+  }
 }
--- src/app/myservice.service.ts
+++ src/app/myservice.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Apollo, gql } from 'apollo-angular';
+
+const USERS = gql`
+  query Users {
+    users {
+      id
+      username
+      city
+      posts {
+        id
+        title
+      }
+    }
+  }
+`;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MyserviceService {
+  constructor(private apollo: Apollo) {}
 
-  constructor() { }
+  getUsers(): Observable<any> {
+    return this.apollo.watchQuery<any>({
+      query: USERS,
+    }).valueChanges;
+  }
 }
